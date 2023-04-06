23b4c562df1ad26683f18d33b423429f220457da
saulo.maker <saulosousasilva3@gmail.com>
2023-04-06 20:46:05 -0300

    Criado interface login inicial do app

--- app/src/main/java/com/saulodev/river/MainActivity.kt
+++ app/src/main/java/com/saulodev/river/MainActivity.kt
@@ -3,14 +3,15 @@ package com.saulodev.river
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.saulodev.river.ui.theme.RiverTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,8 +43,25 @@ fun Main() {
             .fillMaxWidth(),
         color = MaterialTheme.colors.background
     ) {
-        Column() {
-            TextField(value = "", onValueChange = {}, placeholder = { Text(text = "Nome")})
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val imageSize = Modifier.size(200.dp)
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = imageSize
+            )
+            TextField(
+                value = "", onValueChange = {},
+                placeholder = { Text(text = "Nome:")})
+            Spacer(modifier = Modifier.size(10.dp))
+            TextField(value = "", onValueChange = {}, placeholder = { Text(text = "Senha:")})
+            Spacer(modifier = Modifier.size(10.dp))
+            Button(onClick = {  }) {
+                Text(text = "Acessar")
+            }
         }
 
     }
